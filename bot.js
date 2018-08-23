b96a9864dd879992ed83506a0bc3fd49ef646f6f
iiMrMoGreM <40735508+iiMrMoGreM@users.noreply.github.com>
2018-08-23 12:29:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,38 +72,41 @@ client.on('ready', () => {
 
 
 
+
+
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`in ${client.guilds.size} servers `)
     console.log(`[Codes] ${client.users.size}`)
     client.user.setStatus("idle")
 });
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-const prefix = "*"
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
+const prefix = "$"
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
-	//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+	//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 	if (!msg.content.startsWith(prefix)) return undefined;
 	const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
-	//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+	//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
 	const serverQueue = queue.get(msg.guild.id);
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 	let command = msg.content.toLowerCase().split(" ")[0];
 	command = command.slice(prefix.length)
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 	if (command === `play`) {
 		const voiceChannel = msg.member.voiceChannel;
 		if (!voiceChannel) return msg.channel.send('يجب توآجد حضرتك بروم صوتي .');
 		const permissions = voiceChannel.permissionsFor(msg.client.user);
 		if (!permissions.has('CONNECT')) {
-			//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+			//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
-		}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		if (!permissions.has('SPEAK')) {
 			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
-		}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 
 		if (!permissions.has('EMBED_LINKS')) {
 			return msg.channel.sendMessage("**يجب توآفر برمشن `EMBED LINKS`لدي **")
@@ -112,25 +115,25 @@ client.on('message', async msg => { // eslint-disable-line
 		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
 			const playlist = await youtube.getPlaylist(url);
 			const videos = await playlist.getVideos();
-			//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+			//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 			for (const video of Object.values(videos)) {
 				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
 				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
-			}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+			}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 			return msg.channel.send(` **${playlist.title}** تم الإضآفة إلى قأئمة التشغيل`);
 		} else {
-			try {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+			try {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 
 				var video = await youtube.getVideo(url);
 			} catch (error) {
-				try {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+				try {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 					var videos = await youtube.searchVideos(searchString, 5);
 					let index = 0;
 					const embed1 = new Discord.RichEmbed()
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-					.setFooter("CODES")
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+					.setFooter("iiA7TrAf")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
@@ -139,7 +142,7 @@ ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 							maxMatches: 1,
 							time: 15000,
 							errors: ['time']
-						});//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+						});//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games'
 					} catch (err) {
 						console.error(err);
 						return msg.channel.send('لم يتم إختيآر مقطع صوتي');
@@ -150,16 +153,16 @@ ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 					console.error(err);
 					return msg.channel.send(':X: لا يتوفر نتآئج بحث ');
 				}
-			}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+			}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 
 			return handleVideo(video, msg, voiceChannel);
-		}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 	} else if (command === `skip`) {
 		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
 		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لتجآوزه');
 		serverQueue.connection.dispatcher.end('تم تجآوز هذآ المقطع');
 		return undefined;
-	} else if (command === `stop`) {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+	} else if (command === `stop`) {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
 		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لإيقآفه');
 		serverQueue.songs = [];
@@ -169,7 +172,7 @@ ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
 		if (!serverQueue) return msg.channel.send('لا يوجد شيء شغآل.');
 		if (!args[1]) return msg.channel.send(`:loud_sound: مستوى الصوت **${serverQueue.volume}**`);
-		serverQueue.volume = args[1];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		serverQueue.volume = args[1];//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
 		return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
 	} else if (command === `np`) {
@@ -178,12 +181,12 @@ ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 	.setDescription(`:notes: الان يتم تشغيل : **${serverQueue.songs[0].title}**`)
 		return msg.channel.sendEmbed(embedNP);
 	} else if (command === `queue`) {
-		//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
 		let index = 0;
-		//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		const embedqu = new Discord.RichEmbed()
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 .setDescription(`**Songs Queue**
 ${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
 **الان يتم تشغيل** ${serverQueue.songs[0].title}`)
@@ -193,30 +196,30 @@ ${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
 			serverQueue.playing = false;
 			serverQueue.connection.dispatcher.pause();
 			return msg.channel.send('تم إيقاف الموسيقى مؤقتا!');
-		}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		return msg.channel.send('لا يوجد شيء حالي ف العمل.');
 	} else if (command === "resume") {
 		if (serverQueue && !serverQueue.playing) {
 			serverQueue.playing = true;
 			serverQueue.connection.dispatcher.resume();
 			return msg.channel.send('استأنفت الموسيقى بالنسبة لك !');
-		}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		return msg.channel.send('لا يوجد شيء حالي في العمل.');
 	}
 
 	return undefined;
 });
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 async function handleVideo(video, msg, voiceChannel, playlist = false) {
 	const serverQueue = queue.get(msg.guild.id);
 	console.log(video);
-	//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+	//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 //	console.log('yao: ' + Util.escapeMarkdown(video.thumbnailUrl));
 	const song = {
 		id: video.id,
 		title: Util.escapeMarkdown(video.title),
 		url: `https://www.youtube.com/watch?v=${video.id}`
-	};//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+	};//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 	if (!serverQueue) {
 		const queueConstruct = {
 			textChannel: msg.channel,
@@ -225,11 +228,11 @@ async function handleVideo(video, msg, voiceChannel, playlist = false) {
 			songs: [],
 			volume: 5,
 			playing: true
-		};//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		};//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		queue.set(msg.guild.id, queueConstruct);
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		queueConstruct.songs.push(song);
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		try {
 			var connection = await voiceChannel.join();
 			queueConstruct.connection = connection;
@@ -239,45 +242,45 @@ async function handleVideo(video, msg, voiceChannel, playlist = false) {
 			queue.delete(msg.guild.id);
 			return msg.channel.send(`لا أستطيع دخول هذآ الروم ${error}`);
 		}
-	} else {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+	} else {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		serverQueue.songs.push(song);
 		console.log(serverQueue.songs);
 		if (playlist) return undefined;
 		else return msg.channel.send(` **${song.title}** تم اضافه الاغنية الي القائمة!`);
 	}
 	return undefined;
-}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 
 function play(guild, song) {
 	const serverQueue = queue.get(guild.id);
 
-	if (!song) {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+	if (!song) {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 		serverQueue.voiceChannel.leave();
 		queue.delete(guild.id);
-		return;//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-	}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		return;//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+	}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 	console.log(serverQueue.songs);
-//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-		.on('end', reason => {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		.on('end', reason => {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
 			else console.log(reason);
-			serverQueue.songs.shift();//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+			serverQueue.songs.shift();//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 			play(guild, serverQueue.songs[0]);
-		})//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-		.on('error', error => console.error(error));//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+		})//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+		.on('error', error => console.error(error));//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games  '
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 
 	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
-}//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+}//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
 
-const adminprefix = "$vip";//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-const devs = ['274923685985386496'];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-client.on('message', message => {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-  var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-    if (!devs.includes(message.author.id)) return;//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+const adminprefix = "$vip";//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+const devs = ['274923685985386496'];//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+client.on('message', message => {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+  var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+    if (!devs.includes(message.author.id)) return;//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
     
-if (message.content.startsWith(adminprefix + 'setgame')) {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+if (message.content.startsWith(adminprefix + 'setgame')) {//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
@@ -299,8 +302,8 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 client.on("message", message => {
  if (message.content === `${prefix}`) {
-  const embed = new Discord.RichEmbed() //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-      .setColor("#000000")//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+  const embed = new Discord.RichEmbed() //by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+      .setColor("#000000")//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
       .setDescription(`
 ${prefix}play ⇏ لتشغيل أغنية برآبط أو بأسم
 ${prefix}skip ⇏ لتجآوز الأغنية الحآلية
@@ -311,14 +314,13 @@ ${prefix}stop ⇏ لإخرآج البوت من الروم
 ${prefix}np ⇏ لمعرفة الأغنية المشغلة حآليا
 ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
 
- `)//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-   message.channel.sendEmbed(embed)//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+ `)//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
+   message.channel.sendEmbed(embed)//by ,$ ReBeL ء , 🔕#4777 'iiA7TrAf Games '
     
    }
-   }); 
-   
+   });
    
 
   
-
+   
 client.login(process.env.BOT_TOKEN);
